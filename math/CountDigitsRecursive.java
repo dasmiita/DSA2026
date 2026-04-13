0ee835a05db55db8473d59da105f9c6306412dd4
smita <smitu2005noida@gmail.com>
2026-04-13 10:16:46 +0530

    fixed a question, added two methods in counting digits

--- math/CountDigitsRecursive.java
+++ math/CountDigitsRecursive.java
@@ -3,9 +3,12 @@ public class CountDigitsRecursive {
      if(n/10==0) return 1;
      return 1+count(n/10);
     }
-    public 
+    public static int countUsingLog(int n){
+        return (int)Math.floor(Math.log10(n)+1);
+    }
     public static void main(String[] args){
         int n = 343434;
+        System.out.println(countUsingLog(n));
         System.out.println(count(n));
     }
     

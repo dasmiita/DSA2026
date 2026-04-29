92eea2fb24b1fd59395f18754b3a847addf2aeb3
smita <smitu2005noida@gmail.com>
2026-04-29 11:37:16 +0530

    recursion basics 2

---
+++ recursion/rec3.java
@@ -0,0 +1,13 @@
+//package dsa-self.recursion;
+//print 1 to n
+public class rec3 {
+    static void fun(int n ){
+        if(n==0) return;
+        fun(n-1);
+        System.out.println(n);
+    }
+    public static void main(String[] args) {
+        fun(10);
+    }
+    
+}
